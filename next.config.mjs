ba14a6e385dec2b0a6f5606c40212d733cc1b542
mt-abdullah <iammtabdullah@gmail.com>
2026-07-06 14:51:24 +0530

    Clean up config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
