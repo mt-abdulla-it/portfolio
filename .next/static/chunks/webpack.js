7fe5c983fc91f5333f3a8d22be057daa2f7c9681
mt-abdullah <iammtabdullah@gmail.com>
2026-07-10 19:28:45 +0530

    chore: initialize Next.js build artifacts and webpack configuration files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac8ddf564cde2d85"; }
+/******/ 		__webpack_require__.h = function() { return "a88a945ea1f207cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
